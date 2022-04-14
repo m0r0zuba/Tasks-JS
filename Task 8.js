4aae121808de350702dac213a0cd068dfe6eeeec
m0r0zuba <yvasay2002@gmail.com>
2022-04-14 15:15:13 +0300

    test

---
+++ Task 8.js
@@ -0,0 +1,5 @@
+let apples = new Map([["apple1", "red"], ["apple2", "green"], ["apple3", "yellow"]]);
+for (let entity of apples)
+{
+    console.log("Ключ:" , entity[0], "Значение:", entity[1]);   
+}
